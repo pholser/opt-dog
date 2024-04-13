f19e67ebebfad36698cac371d6d60a66bbf27213
Paul Holser <paul.holser@aclaimant.com>
2024-04-13 16:22:23 -0500

    parsing breed counts. next: use these to generate entrants for breed events

--- optdog/ingestion/bradshaw/breed_counts.py
+++ optdog/ingestion/bradshaw/breed_counts.py
@@ -1,14 +1,53 @@
 from bs4 import BeautifulSoup
+from more_itertools import partition
 import re
+
+
+def parse_counts(raw):
+    no_parens = raw.replace('(', '').replace(')', '')
+    pieces = no_parens.split('-')
+    counts_by_type = {}
+
+    others, veteran_dog_pieces = partition(lambda p: 'VD' in p, pieces)
+    others = list(others)
+    veteran_dog_pieces = list(veteran_dog_pieces)
+    if veteran_dog_pieces:
+        counts_by_type['veteran-dog'] = int(veteran_dog_pieces[0].replace('VD', ''))
+        pieces = others
+    others, veteran_bitch_pieces = list(partition(lambda p: 'VB' in p, pieces))
+    others = list(others)
+    veteran_bitch_pieces = list(veteran_bitch_pieces)
+    if veteran_bitch_pieces:
+        counts_by_type['veteran-bitch'] = int(veteran_bitch_pieces[0].replace('VB', ''))
+        pieces = others
+    if len(pieces) == 2:
+        counts_by_type['class-dog'] = int(pieces[0])
+        counts_by_type['class-bitch'] = int(pieces[1])
+    elif len(pieces) == 4:
+        counts_by_type['class-dog'] = int(pieces[0])
+        counts_by_type['class-bitch'] = int(pieces[1])
+        counts_by_type['champion-dog'] = int(pieces[2])
+        counts_by_type['champion-bitch'] = int(pieces[3])
+    else:
+        print(f'Warning: could not parse counts string {raw}')
+    return counts_by_type
+
+
+def parse_breed_and_counts(raw):
+    match = re.search(r'\d', raw)
+    index = match.start()
+    breed_name_piece = raw[:index].strip()
+    raw_counts_piece = raw[index:].strip()
+    return breed_name_piece, parse_counts(raw_counts_piece)
 
 
 def extract_from_table(program_table):
     counts = {}
     for row in program_table.find_all('tr'):
         breed_count_cell = row.find('td', string=re.compile(r'\d+-\d+'))
-        if breed_count_cell is None:
-            break
-        print(breed_count_cell.text)
+        if breed_count_cell is not None:
+            breed_name, detail_counts = parse_breed_and_counts(breed_count_cell.text)
+            counts[breed_name] = detail_counts
     return counts
 
 
